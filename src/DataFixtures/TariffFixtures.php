86cf12e1f6a2a566e494e4fb8fc7a82f9cd60ec2
Alex Krol <krolmail@list.ru>
2024-06-20 15:21:46 +0300

    add docker support, fix fixtures load

--- src/DataFixtures/TariffFixtures.php
+++ src/DataFixtures/TariffFixtures.php
@@ -4,18 +4,29 @@ namespace App\DataFixtures;
 
 use App\Entity\Tariff;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
 
 class TariffFixtures extends Fixture
 {
+    public function __construct(
+        private readonly ManagerRegistry $managerRegistry
+    )
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $tariff = new Tariff();
-        $tariff->setName('Базовый');
-        $tariff->setPrice(100);
-        $tariff->setDiscountPercentage(10);
-        $manager->persist($tariff);
+        $tariffRepository = $this->managerRegistry->getRepository(Tariff::class);
 
-        $manager->flush();
+        if (!$tariffRepository->findOneBy([])) {
+            $tariff = new Tariff();
+            $tariff->setName('Базовый');
+            $tariff->setPrice(100);
+            $tariff->setDiscountPercentage(10);
+            $manager->persist($tariff);
+
+            $manager->flush();
+        }
     }
 }
